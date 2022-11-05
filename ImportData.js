5aeedcc273ad6f30560cabf3008bf17f84e16d97
Nodshley Marcelin <90664251+designermarvel@users.noreply.github.com>
2022-11-05 03:24:45 -0400

    AddedCamera

--- ImportData.js
+++ ImportData.js
@@ -5,21 +5,76 @@ import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 export default function ImportData() {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+ 
+  Camera.requestMicrophonePermissionsAsync();
+  Camera.requestCameraPermissionsAsync();
+  
+
 
 
   function toggleCameraType() {
+    
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
+    <View style={styles.container} >
+      <Camera style={styles.view} >
       </Camera>
+
+      
+      <TouchableOpacity style={styles.createbtn} onPress={toggleCameraType}>
+        <Text>
+          Flip</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.createbtn_2} onPress={toggleCameraType}>
+        <Text>
+          Convert</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+  
+    width:'100%',
+    height: 300,
+    
+    
+
+  
+  },
+   createbtn: {
+    color:'#FFFFFF',
+    width: "70%",
+    borderRadius: 100,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
+    backgroundColor: "#481196",
+    marginLeft:50,
+  },
+    createbtn_2: {
+    color:'#FFFFFF',
+    width: "70%",
+    borderRadius: 100,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    backgroundColor: "#481196",
+    marginLeft:50,
+    marginBottom: 150,
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#032130',
+  }
+ 
+
+}
+)
